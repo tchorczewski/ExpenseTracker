f6d3d2d7bba1d8f498feaa1b457612f850b5c565
tchorowski.bartosz@gmail.com <tchorowski.bartosz@gmail.com>
2025-09-24 23:14:43 +0200

    Finishing recent operations table on back and frontend

--- app/routes/dashboard_routes.py
+++ app/routes/dashboard_routes.py
@@ -5,6 +5,7 @@ from app.common.decorators import error_handler
 from app.services.auth_services import get_auth_user
 from db import db
 from db.models import Expenses, ExpenseCategories, Incomes, IncomeCategories
+from utils.mappers import last_operations_mapper
 
 dashboard_bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 
@@ -23,7 +24,7 @@ def get_last_operations():
             ExpenseCategories.category_name,
         )
         .join(ExpenseCategories, Expenses.category_id == ExpenseCategories.category_id)
-        .where(Incomes.user_id == user.user_id)
+        .where(Expenses.user_id == user.user_id)
     )
 
     last_incomes_query = (
@@ -32,8 +33,8 @@ def get_last_operations():
             Incomes.income_date.label("date"),
             IncomeCategories.category_name,
         )
-        .join(Incomes, Incomes.income_date.label("income_date"))
-        .where(Expenses.user_id == user.user_id)
+        .join(IncomeCategories, Incomes.category_id == IncomeCategories.category_id)
+        .where(Incomes.user_id == user.user_id)
     )
 
     combined = union_all(last_expenses_query, last_incomes_query).subquery("combined")
@@ -43,30 +44,15 @@ def get_last_operations():
             combined.c.amount,
             combined.c.date,
             combined.c.category_name,
-            combined.c.type,
         )
         .order_by(combined.c.date.desc())
         .limit(5)
     )
 
-    result = db.session.execute(last_operations_stmt)
+    result = db.session.execute(last_operations_stmt).all()
 
-    return jsonify(result), 201
+    if not result:
+        return None, 201
 
-
-"""
-#TODO COnvert this stuff
-SELECT * FROM 
-(SELECT expenses.amount, expense_date as date, expense_categories.category_name FROM expenses
-INNER JOIN expense_categories ON expenses.category_id = expense_categories.category_id
-WHERE user_id = 1
-
-UNION ALL
-
-SELECT incomes.amount, income_date as date, income_categories.category_name FROM incomes
-INNER JOIN income_categories ON incomes.category_id = income_categories.category_id
-WHERE user_id = 1)  as combined
-Order BY date DESC
-Limit 5
-into an endpoint to call
-"""
+    result_list = [last_operations_mapper(item) for item in result]
+    return result_list, 201
